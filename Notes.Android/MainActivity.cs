101f4e444ffb131a1249f34d95622b1174416cb2
Benz0236 <Thanapongk@hatari.net>
2021-04-01 14:55:29 +0700

    01

--- Notes.Android/MainActivity.cs
+++ Notes.Android/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 
+
 namespace Notes.Droid
 {
     [Activity(Label = "Notes", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
